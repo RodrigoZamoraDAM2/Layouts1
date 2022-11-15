eeae3f87ad76c242ad332b3eca32f1c509e70b48
RodrigoZamoraDAM2 <rodzam799@alu.centroafuera.es>
2022-11-15 17:21:42 +0100

    Terminado

--- app/src/main/java/com/example/layouts1/MainActivity.kt
+++ app/src/main/java/com/example/layouts1/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.layouts1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnLinear : Button = findViewById(R.id.Linearbutton)
+        var btnTable : Button = findViewById(R.id.TableButton)
+        var btnConstraint : Button = findViewById(R.id.ConstraintButton)
+
+        btnLinear.setOnClickListener(){
+            startActivity(Intent(this@MainActivity,Linear1::class.java))
+        }
+        btnTable.setOnClickListener(){
+            startActivity(Intent(this@MainActivity,Table1::class.java))
+        }
+        btnConstraint.setOnClickListener(){
+            startActivity(Intent(this@MainActivity,Constraint1::class.java))
+        }
     }
 }
